4e495c51f6e045550c96edae6a827202b6b7213f
IamMrandrew <andrewliyanlap000824@gmail.com>
2020-08-14 00:07:13 +0800

    style: style pagination v2

--- single.php
+++ single.php
@@ -19,8 +19,16 @@ get_header();
 			get_template_part( 'template-parts/content', get_post_type() );
 			
 		?>
-			<?php previous_post_link( '<div class="pre_post_link"><i class="fas fa-angle-left"></i>上一篇%link</div>',  '<div class="pre_post_title">%title</div>', true ); ?>
-			<?php next_post_link( '<div class="next_post_link"><i class="fas fa-angle-right"></i>下一篇 %link</div>',  'div class="next_post_title">%title</div>', true ); ?>
+		<div class="post_links">
+			<div class="col-lg-6 left">
+				<?php previous_post_link( '<div class="pre_post_link"><i class="fas fa-angle-left"></i>上一篇%link</div>',  '<div class="pre_post_title">%title</div>', true ); ?>
+			</div>
+			<div class="col-lg-6 right">
+			<?php next_post_link( '<div class="next_post_link">下一篇<i class="fas fa-angle-right"></i>%link</div>',  '<div class="next_post_title">%title</div>', true ); ?>
+			</div>
+		</div>
+			
+			
 		<?php
 			// the_post_navigation(
 			// 	array(
