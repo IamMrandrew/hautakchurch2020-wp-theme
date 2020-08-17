c416ce2c6d43055ae7172018f074e525d31310e8
IamMrandrew <andrewliyanlap000824@gmail.com>
2020-08-17 16:50:02 +0800

    style: min height of contact text-container

--- template-parts/page-contact.php
+++ template-parts/page-contact.php
@@ -16,7 +16,6 @@
 
 get_header();
 ?>
-
 <div class="container text-container contact">
     <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
@@ -73,82 +72,82 @@ get_header();
     </section>
 </div>
 
-    <div class="map" id="map-2"></div>
+<div class="map" id="map-2"></div>
 
-    <script>
-        function initMap() { 
-            let map2 = new google.maps.Map(document.getElementById('map-2'), {
-                center: { lat: 22.3177851, lng: 114.2663983 },
-                zoom: 17,
-                streetViewControl: false,
-                fullscreenControl: false,
-                // zoomControl: false
-            });
+<script>
+    function initMap() { 
+        let map2 = new google.maps.Map(document.getElementById('map-2'), {
+            center: { lat: 22.3177851, lng: 114.2663983 },
+            zoom: 17,
+            streetViewControl: false,
+            fullscreenControl: false,
+            // zoomControl: false
+        });
 
-            let marker1 = addMarker({ lat: 22.3154014, lng: 114.2672707 });
-            let marker2 = addMarker({ lat: 22.3183149, lng: 114.2660506 });
-            let marker3 = addMarker({ lat: 22.3211118, lng: 114.2665366 });
+        let marker1 = addMarker({ lat: 22.3154014, lng: 114.2672707 });
+        let marker2 = addMarker({ lat: 22.3183149, lng: 114.2660506 });
+        let marker3 = addMarker({ lat: 22.3211118, lng: 114.2665366 });
 
-            let infoWindow1 = addInfoWindow(
-                '<h3 class="title">主日崇拜及親子崇拜</h3>' + 
-                '<h3>陳耀星小學</h3>' + 
-                '<p>九龍將軍澳第二期第三十四區仁濟醫院陳耀星小學(坑口和明苑內) </p>' + 
-                '<p> <i class="fas fa-phone fa-flip-horizontal"></i>2623 6316 </p>'
-            )
-            
-            let infoWindow2 = addInfoWindow(
-                '<h3 class="title">週六崇拜及其他聚會</h3>' + 
-                '<h3>教會幼稚園</h3>' + 
-                '<p>九龍將軍澳厚德村裕明苑裕榮閣地下</p>' + 
-                '<p> <i class="fas fa-phone fa-flip-horizontal"></i>2623 6316 </p>'
-            )
+        let infoWindow1 = addInfoWindow(
+            '<h3 class="title">主日崇拜及親子崇拜</h3>' + 
+            '<h3>陳耀星小學</h3>' + 
+            '<p>九龍將軍澳第二期第三十四區仁濟醫院陳耀星小學(坑口和明苑內) </p>' + 
+            '<p> <i class="fas fa-phone fa-flip-horizontal"></i>2623 6316 </p>'
+        )
+        
+        let infoWindow2 = addInfoWindow(
+            '<h3 class="title">週六崇拜及其他聚會</h3>' + 
+            '<h3>教會幼稚園</h3>' + 
+            '<p>九龍將軍澳厚德村裕明苑裕榮閣地下</p>' + 
+            '<p> <i class="fas fa-phone fa-flip-horizontal"></i>2623 6316 </p>'
+        )
 
-            let infoWindow3 = addInfoWindow(
-                '<h3 class="title">辦公室</h3>' + 
-                '<p>九龍將軍澳坑口村11號B地下</p>' + 
-                '<p> <i class="fas fa-phone fa-flip-horizontal"></i>2623 6316 </p>'
-            )
+        let infoWindow3 = addInfoWindow(
+            '<h3 class="title">辦公室</h3>' + 
+            '<p>九龍將軍澳坑口村11號B地下</p>' + 
+            '<p> <i class="fas fa-phone fa-flip-horizontal"></i>2623 6316 </p>'
+        )
 
+        infoWindow1.open(map2, marker1);
+        infoWindow2.open(map2, marker2);
+        infoWindow3.open(map2, marker3);
+
+        marker1.addListener('click', function() {
             infoWindow1.open(map2, marker1);
+        });
+
+
+        marker2.addListener('click', function() {
             infoWindow2.open(map2, marker2);
+        });
+
+
+        marker3.addListener('click', function() {
             infoWindow3.open(map2, marker3);
+        });
 
-            marker1.addListener('click', function() {
-                infoWindow1.open(map2, marker1);
+        function addInfoWindow(content) {
+            let infoWindow = new google.maps.InfoWindow({
+            content: content
             });
 
-
-            marker2.addListener('click', function() {
-                infoWindow2.open(map2, marker2);
-            });
-
-
-            marker3.addListener('click', function() {
-                infoWindow3.open(map2, marker3);
-            });
-
-            function addInfoWindow(content) {
-                let infoWindow = new google.maps.InfoWindow({
-                content: content
-                });
-
-                return infoWindow;
-            }
-            
-            function addMarker(coord) {
-                let marker = new google.maps.Marker({
-                    position: coord,
-                    map: map2,
-                    icon: '<?php echo get_template_directory_uri() ?>/img/marker.svg'
-                });
-
-                return marker;
-            }
+            return infoWindow;
         }
-    </script>
-    <script defer
-        src="https://maps.googleapis.com/maps/api/js?key=<?php echo $apikey ?>&callback=initMap">
-    </script>
+        
+        function addMarker(coord) {
+            let marker = new google.maps.Marker({
+                position: coord,
+                map: map2,
+                icon: '<?php echo get_template_directory_uri() ?>/img/marker.svg'
+            });
+
+            return marker;
+        }
+    }
+</script>
+<script defer
+    src="https://maps.googleapis.com/maps/api/js?key=<?php echo $apikey ?>&callback=initMap">
+</script>
     
 <?php
 
