47bfb5ad4ddc8521d94cd1322d5215c8fd0b4083
IamMrandrew <andrewliyanlap000824@gmail.com>
2020-08-19 18:43:29 +0800

    feat: timetable add thumbnail and clickable permalink

--- template-parts/content-timetable.php
+++ template-parts/content-timetable.php
@@ -10,17 +10,20 @@
 ?>
 
 <div class="col-lg-6">
-    <div class="card">
-        <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="thumbnail">
-        <div class="info-wrapper">
-            <div class="text-wrapper">
-                <h3 class="title"><?php the_title() ?></h3>       
-                <p class="time"><?php echo get_post_meta(get_the_ID(), '_time_value_key', true) ?></p>
-            </div>
-            <div class="text-wrapper">
-                <p class="location"><?php echo get_post_meta(get_the_ID(), '_location_value_key', true) ?></p> 
-                <p class="person"><?php echo get_post_meta(get_the_ID(), '_target_value_key', true) ?></p>
-            </div>
+<div class="card">
+<a class="card-link" href="<?php the_permalink()?>">
+    <div class="img-wrapper">
+        <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="thumbnail"></div>
+    <div class="info-wrapper">
+        <div class="text-wrapper">
+            <h3 class="title"><?php the_title() ?></h3>       
+            <p class="time"><?php echo get_post_meta(get_the_ID(), '_time_value_key', true) ?></p>
+        </div>
+        <div class="text-wrapper">
+            <p class="location"><?php echo get_post_meta(get_the_ID(), '_location_value_key', true) ?></p> 
+            <p class="person"><?php echo get_post_meta(get_the_ID(), '_target_value_key', true) ?></p>
         </div>
     </div>
+</a>
+</div>
 </div> 
