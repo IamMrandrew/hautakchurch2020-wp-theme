462cba6336d4ee31f855a5feae64f0461917118c
IamMrandrew <andrewliyanlap000824@gmail.com>
2020-08-19 20:34:56 +0800

    style (gallery): styled gallery plugin

--- js/page.js
+++ js/page.js
@@ -2,3 +2,8 @@ const downloadButton = document.querySelector('.wp-block-file__button');
 
 if (downloadButton)
     downloadButton.textContent="下載";
+
+const breadcrumbMain = document.querySelector('.aigpl-breadcrumb');
+
+if (breadcrumbMain)
+    breadcrumbMain.textContent="主圖片庫";
