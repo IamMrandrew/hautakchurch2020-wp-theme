2e61c5cbd0c34c23fd4f05652e4209ebe8d7657f
IamMrandrew <andrewliyanlap000824@gmail.com>
2020-08-19 18:13:40 +0800

    refactor: remove plyr scss

--- template-parts/content-timetable.php
+++ template-parts/content-timetable.php
@@ -10,10 +10,17 @@
 ?>
 
 <div class="col-lg-6">
-    <h3><?php the_title() ?></h3>
-    <p><?php echo get_post_meta(get_the_ID(), '_time_value_key', true) ?></p>
-    <p>
-        <span class="location"><?php echo get_post_meta(get_the_ID(), '_location_value_key', true) ?></span> 
-        <span class="person"><?php echo get_post_meta(get_the_ID(), '_target_value_key', true) ?></span>
-    </p>
+    <div class="card">
+        <img src="<?php echo get_the_post_thumbnail_url() ?>" alt="thumbnail">
+        <div class="info-wrapper">
+            <div class="text-wrapper">
+                <h3 class="title"><?php the_title() ?></h3>       
+                <p class="time"><?php echo get_post_meta(get_the_ID(), '_time_value_key', true) ?></p>
+            </div>
+            <div class="text-wrapper">
+                <p class="location"><?php echo get_post_meta(get_the_ID(), '_location_value_key', true) ?></p> 
+                <p class="person"><?php echo get_post_meta(get_the_ID(), '_target_value_key', true) ?></p>
+            </div>
+        </div>
+    </div>
 </div> 
