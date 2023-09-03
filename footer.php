c07855fefb634baccf293e024b93b4f167733727
Andrew Li <andrewliyanlap000824@gmail.com>
2023-09-04 00:31:43 +0800

    🐞Remove comment in footer.php to resolve parse error

--- footer.php
+++ footer.php
@@ -25,24 +25,3 @@
 
 </body>
 </html>
-<!-- 
-<footer id="colophon" class="site-footer bg-dark border-top" style="height: 50px">
-		<div class="site-info">
-		<div class="container header-container" style="padding-top: 12px; max-width: 1200px">
-			<div class="text-left" style="font-size: 14px; color: #CBCBCB"> -->
-			
-			<!-- </div>
-			<a href="<?php /* echo esc_url( __( 'https://wordpress.org/', 'hautakchurch' ) ); ?>">
-			 	<?php 
-				/* translators: %s: CMS name, i.e. WordPress. */ /*
-				printf( esc_html__( 'Proudly powered by %s', 'hautakchurch' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */ /*
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'hautakchurch' ), 'hautakchurch', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div> -->
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
